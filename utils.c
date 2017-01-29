cbd531a44fdbcc150b932f8c39672d93db039b69
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-01-29 18:31:03 -0500

    Add files via upload

--- utils.c
+++ utils.c
@@ -51,6 +51,6 @@ static int convert_to_records(char * filename, struct record * buffer[]){
 }
 
 int main(int argc, char **argv){
-	struct Record** buffer [1024];
+	struct Record* buffer [1024];
 	convert_to_records(argv[1], buffer);	
 }
