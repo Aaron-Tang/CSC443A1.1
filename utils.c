9676291413156ede2fa3dd62084999f1c9abdc8f
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-01-29 16:23:58 -0500

    Add files via upload

--- utils.c
+++ utils.c
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include "utils.h"
 
-struct Record * convert_to_record(char * line[]){
-	struct Record * new_record = (struct Record *) malloc(sizeof(struct Record));
-	new_record->uid1 = strtok(line, ",");
-	new_record->uid2 = strtok(0, ",");
+typedef struct record {
+ int uid1;
+ int uid2;
+} Record;
 
+const int MAX_CHARS_PER_LINE = 1024;
+
+struct record convert_to_record(char line[]){
+
+	char * uid1 = strtok(line, ",");
+	char * uid2 = strtok(NULL, ",");
+	struct record new_record = {atoi(uid1), atoi(uid2)};
 	return new_record;
 
 }
 
-static int convert_to_records(const char * filename){
+static int convert_to_records(char * filename){
 	/* returns: -1 if there is an error. */
 	
 	char current_line[MAX_CHARS_PER_LINE];
@@ -21,7 +27,7 @@ static int convert_to_records(const char * filename){
 	FILE *fp_read;
 	
 	/* open text file for reading */
-	if (!(fp_read= fopen ( filename , "r" ))) {
+	if (!(fp_read= fopen(filename, "r"))) {
 		printf ("Could not open file \"%s\" for reading \n", filename);
 		return (-1);
 	}
@@ -29,7 +35,7 @@ static int convert_to_records(const char * filename){
 	/* reading lines */
 	while( fgets (current_line, MAX_CHARS_PER_LINE, fp_read)!=NULL ) {
 		current_line [strcspn (current_line, "\r\n")] = '\0'; //remove end-of-line characters
-
+		convert_to_record(current_line);
 	}
 	
 	fclose (fp_read);
@@ -37,3 +43,6 @@ static int convert_to_records(const char * filename){
 	return 0;
 }
 
+int main(int argc, char **argv){
+	convert_to_records("edges.csv");	
+}
