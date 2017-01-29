9b385eccaf3905edeb2b67d473afdade294210fc
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-01-29 14:24:08 -0500

    Add files via upload

---
+++ utils.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "utils.h"
+
+static int convert_to_records(const char * filename){
+	/* returns: -1 if there is an error. */
+	
+	char current_line[MAX_CHARS_PER_LINE];
+
+	FILE *fp_read;
+	
+	/* open text file for reading */
+	if (!(fp_read= fopen ( filename , "r" ))) {
+		printf ("Could not open file \"%s\" for reading \n", filename);
+		return (-1);
+	}
+	     
+	/* reading lines */
+	while( fgets (current_line, MAX_CHARS_PER_LINE, fp_read)!=NULL ) {
+		current_line [strcspn (current_line, "\r\n")] = '\0'; //remove end-of-line characters
+
+	}
+	
+	fclose (fp_read);
+
+	return 0;
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+typedef struct record {
+ int uid1;
+ int uid2;
+} Record;
+
+const MAX_CHARS_PER_LINE = 1024;
+
+void convert_to_record(const char * filename)
