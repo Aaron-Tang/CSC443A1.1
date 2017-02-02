2bc41acbe8029c52418c6b489c3e4ce51402640b
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-02-01 19:55:34 -0500

    Add files via upload

--- utils.h
+++ utils.h
@@ -1,3 +1,6 @@
+#ifndef _utils_h
+#define _utils_h
+
 typedef struct record {
  int uid1;
  int uid2;
@@ -7,3 +10,5 @@ const int MAX_CHARS_PER_LINE = 1024;
 const int MB = 1024 * 1024;
 
 void convert_to_record(char line[], Record * current_record);
+
+#endif
