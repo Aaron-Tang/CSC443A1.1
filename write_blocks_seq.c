05ad80312373c09de59a9cb12357a2ef65460f4d
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-02-22 14:47:01 -0500

    Add files via upload

--- write_blocks_seq.c
+++ write_blocks_seq.c
@@ -13,11 +13,8 @@ int write_blocks_seq(char * filename, int blocksize){
 	   
 	int block_size = blocksize;
 	int records_per_block = block_size/sizeof(Record);
-	printf("%d\n", records_per_block);
 	char current_line[MAX_CHARS_PER_LINE];
-	int total_records = 0;
-	printf("total_records, %d\n", total_records);
-
+	int total_records = 0, in_buff = 0;
 
 	FILE *fp_read;
 	FILE *fp_write;
@@ -40,37 +37,30 @@ int write_blocks_seq(char * filename, int blocksize){
 
 	/* reading lines */
 	while(fgets(current_line, MAX_CHARS_PER_LINE, fp_read) !=NULL ) {
-		current_line [strcspn (current_line, "\r\n")] = '\0'; //remove end-of-line characters
-		Record * current = (Record *) malloc(sizeof(Record));
-		convert_to_record(current_line, current);
+		if (current_line[0] != '\0'){
+			current_line [strcspn (current_line, "\r\n")] = '\0'; //remove end-of-line characters
+			Record * current = (Record *) malloc(sizeof(Record));
+			convert_to_record(current_line, current);
 
-		printf("THERE\n");
-		if (total_records == records_per_block || feof(fp_read)){
-			printf("NOW HERE\n");
-			fwrite (buffer, sizeof(Record), total_records, fp_write);
-			/* force data to disk */
-			fflush (fp_write); 
-			printf("MAYBE?\n");
-			total_records = 0;
+			if (in_buff == records_per_block){
+				fwrite (buffer, sizeof(Record), total_records, fp_write);
+				memset(buffer, 0, sizeof(*buffer));
+				in_buff = 0;
 
+			}
+
+			memcpy(&buffer[in_buff], current, sizeof(Record));
+			total_records_time ++;
+			total_records = total_records + 1;
+			free(current);
 		}
-
-			/* flush buffer when full */
-
-		printf("HERE\n");	
-		buffer[total_records] = *current;
-		free(current);
-		total_records = total_records + 1;
-		total_records_time ++;
 	}
 	
-
-
+	fwrite (buffer, sizeof(Record), total_records, fp_write);
+	fflush (fp_write); 
+	free (buffer);
 	fclose(fp_write);
 	fclose (fp_read);
-
-	free (buffer);
-
 
 	ftime(&t_end);     
 	 
